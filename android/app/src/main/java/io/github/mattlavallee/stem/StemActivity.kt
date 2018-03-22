5691ac200b1b401ca2e25929c254cfdb57958fe1
Matt Lavallee <matthew.t.lavallee@gmail.com>
2018-03-21 20:15:05 -0400

    feat(bottom-sheet): initial implementation of bottom sheet for join
    This still needs a ton of work, but is in a not-so-shitty state currently

--- android/app/src/main/java/io/github/mattlavallee/stem/StemActivity.kt
+++ android/app/src/main/java/io/github/mattlavallee/stem/StemActivity.kt
@@ -2,11 +2,13 @@ package io.github.mattlavallee.stem
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_stem.*
 
 class StemActivity : AppCompatActivity() {
-
+    private var bottomSheetBehavior: BottomSheetBehavior<*>? = null
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -15,6 +17,7 @@ class StemActivity : AppCompatActivity() {
             }
             R.id.navigation_join -> {
                 message.setText(R.string.nav_join)
+                bottomSheetBehavior?.state = BottomSheetBehavior.STATE_EXPANDED
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_create -> {
@@ -29,6 +32,18 @@ class StemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stem)
 
+        val bottomSheet = findViewById<LinearLayout>(R.id.bottom_sheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onBackPressed() {
+        if (bottomSheetBehavior != null && bottomSheetBehavior?.state == BottomSheetBehavior.STATE_EXPANDED) {
+            bottomSheetBehavior?.state = BottomSheetBehavior.STATE_HIDDEN;
+        } else {
+            super.onBackPressed();
+        }
     }
 }
