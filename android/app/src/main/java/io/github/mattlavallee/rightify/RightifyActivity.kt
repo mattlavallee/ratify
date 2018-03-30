b1ec19cbb49b86cc4327af721773e6c304a0d8a7
Matt Lavallee <matthew.t.lavallee@gmail.com>
2018-03-30 11:32:42 -0400

    feat(layout): adding fragments for home and create views

--- android/app/src/main/java/io/github/mattlavallee/rightify/RightifyActivity.kt
+++ android/app/src/main/java/io/github/mattlavallee/rightify/RightifyActivity.kt
@@ -3,10 +3,14 @@ package io.github.mattlavallee.rightify
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.BottomSheetBehavior
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import io.github.mattlavallee.rightify.presentation.CreateFragment
+import io.github.mattlavallee.rightify.presentation.HomeFragment
 import io.github.mattlavallee.rightify.presentation.JoinView
 import kotlinx.android.synthetic.main.activity_rightify.*
 
@@ -14,10 +18,10 @@ class RightifyActivity : AppCompatActivity() {
     private var bottomSheetBehavior: BottomSheetBehavior<*>? = null
     private var joinViewModel: JoinView? = null
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.nav_home)
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = HomeFragment()
             }
             R.id.navigation_join -> {
                 joinViewModel?.resetCodeInput()
@@ -25,11 +29,14 @@ class RightifyActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener false
             }
             R.id.navigation_create -> {
-                message.setText(R.string.nav_create)
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = CreateFragment()
             }
         }
-        false
+
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.content_container, selectedFragment)
+        transaction.commit()
+        return@OnNavigationItemSelectedListener true
     }
 
     private fun initJoinView() {
@@ -41,8 +48,16 @@ class RightifyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rightify)
+
+        //initialize the default home fragment
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.content_container, HomeFragment())
+        transaction.commit()
+
+        //initialize the join bottomsheet
         initJoinView()
 
+        //TODO: move this to JoinView
         val bottomSheet = findViewById<LinearLayout>(R.id.bottom_sheet)
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
---
+++ android/app/src/main/java/io/github/mattlavallee/rightify/presentation/CreateFragment.kt
@@ -0,0 +1,18 @@
+package io.github.mattlavallee.rightify.presentation
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.mattlavallee.rightify.R
+
+class CreateFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_create, container, false)
+    }
+}
