64a306722fb98f0857f04324a5219241de9ca01a
Matt Lavallee <matthew.t.lavallee@gmail.com>
2018-05-01 18:54:08 -0400

    feat(server): scaffolding routes for groups

--- server/server/server.ts
+++ server/server/server.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import {config as dotenvConfig} from 'dotenv';
 import * as bodyParser from 'body-parser';
 import registerHomeRoutes from './routes/home';
+import registerGroupRoutes from './routes/group';
 
 dotenvConfig();
 
@@ -11,7 +12,14 @@ app.set('port', process.env.PORT || 8000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-registerHomeRoutes(app);
+const routeRegistration: Function[] = [
+  registerHomeRoutes,
+  registerGroupRoutes,
+];
+
+for(let i = 0; i < routeRegistration.length; i++) {
+  routeRegistration[i](app);
+}
 
 app.listen(app.get('port'), () => {
   console.log(('App is running at http://localhost:%d in %s mode'),
