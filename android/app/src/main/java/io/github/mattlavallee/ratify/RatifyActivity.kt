2994951c77e0a97f649736d92bfea46f3186e601
Matt Lavallee <matthew.t.lavallee@gmail.com>
2018-05-16 13:35:42 -0400

    fix(auth): getting google auth working properly

--- android/app/src/main/java/io/github/mattlavallee/ratify/RatifyActivity.kt
+++ android/app/src/main/java/io/github/mattlavallee/ratify/RatifyActivity.kt
@@ -1,5 +1,6 @@
 package io.github.mattlavallee.ratify
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
@@ -11,12 +12,19 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import io.github.mattlavallee.ratify.presentation.CreateFragment
 import io.github.mattlavallee.ratify.presentation.HomeFragment
 import io.github.mattlavallee.ratify.presentation.JoinView
+import io.github.mattlavallee.ratify.presentation.SnackbarGenerator
 import kotlinx.android.synthetic.main.activity_ratify.*
+import java.util.*
 
 class RatifyActivity : AppCompatActivity() {
+    private val RC_SIGN_IN: Int = 123
     private var bottomSheetBehavior: BottomSheetBehavior<*>? = null
     private var joinViewModel: JoinView? = null
     private var selectedFragment: Fragment? = null
@@ -66,6 +74,17 @@ class RatifyActivity : AppCompatActivity() {
         bottomSheetBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        var providers: List<AuthUI.IdpConfig> = Arrays.asList(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        //create and launch the sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                  .createSignInIntentBuilder()
+                  .setAvailableProviders(providers)
+                  .build(),
+            RC_SIGN_IN)
     }
 
     override fun onResume() {
@@ -84,5 +103,18 @@ class RatifyActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
         selectedFragment?.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            var response: IdpResponse = IdpResponse.fromResultIntent(data) as IdpResponse
+            if (resultCode == Activity.RESULT_OK) {
+                var user: FirebaseUser = FirebaseAuth.getInstance().currentUser as FirebaseUser
+                val baseView = findViewById<View>(android.R.id.content)
+                SnackbarGenerator.generateSnackbar(baseView, "User: " + user.displayName)?.show()
+            } else {
+                //sign in failed
+                val baseView = findViewById<View>(android.R.id.content)
+                SnackbarGenerator.generateSnackbar(baseView, "Error signing in...")?.show()
+            }
+        }
     }
 }
