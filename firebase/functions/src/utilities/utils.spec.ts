1b6d300ca8a6ffec0857d85944901bf8152bf00f
Matt Lavallee <matthew.t.lavallee@gmail.com>
2018-10-08 21:21:27 -0400

    chore: fixing linting issues

--- firebase/functions/src/utilities/utils.spec.ts
+++ firebase/functions/src/utilities/utils.spec.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import {GroupRequest} from '../models/group';
 import { generateUuid } from './uuid';
 import {getYelpResultsForGroup} from './yelp-requester';
-import {YelpResult} from '../models/yelp-result';
 
 describe('Utility Functions', () => {
     describe('UUID', () => {
@@ -19,7 +18,7 @@ describe('Utility Functions', () => {
             const date = new Date();
             const testGroup = new GroupRequest('test', 'restaurant', 'test', 
                 'lunch', 'here', 1, 1, 10, date.getTime(), date.getTime());
-            getYelpResultsForGroup(testGroup);
+            getYelpResultsForGroup(testGroup).then().catch();
 
             expect(stub.mock.calls[0][0]).toEqual(
                 'https://api.yelp.com/v3/businesses/search?radius=4000&term=lunch&latitude=1&longitude=1&limit=10&sort_by=distance'
@@ -57,8 +56,11 @@ describe('Utility Functions', () => {
                 'lunch', 'here', 1, 1, 10, date.getTime(), date.getTime());
             getYelpResultsForGroup(testGroup).then(results => {
                 expect(results.length).toEqual(2);
-                expect((results[0] as YelpResult).name).toEqual('Foo Name');
-                expect((results[1] as YelpResult).name).toEqual('Bar Name');
+                expect(results[0].name).toEqual('Foo Name');
+                expect(results[1].name).toEqual('Bar Name');
+                done();
+            }).catch(() => {
+                expect(true).toEqual(false);
                 done();
             });
         });
