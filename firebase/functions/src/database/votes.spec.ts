dcf518fb97c4d6f0f9803d6ffbecc86062df19dd
Matt Lavallee <matthew.t.lavallee@gmail.com>
2018-10-23 21:29:46 -0400

    test(votes): adding unit tests for votes db handler

--- firebase/functions/src/database/votes.spec.ts
+++ firebase/functions/src/database/votes.spec.ts
@@ -1,47 +1,86 @@
 import * as dbInstance from './db-instance';
-// let returnVal = false;
-// const dbMock = {
-//   ref: () => {
-//     return {
-//       limitToFirst: () => {
-//         return {
-//           once: () => Promise.resolve({
-//             val: () => {
-//               return returnVal ? {} : null;
-//             },
-//           }),
-//         };
-//       },
-//       set: () => Promise.resolve(),
-//       child: () => {
-//         return {
-//           set: () => Promise.resolve(),
-//         };
-//       }
-//     };
-//   },
-// };
+
+let childOnceHasInstance = true;
+let limitOnceHasInstance = true;
+const dbMock = {
+  ref: () => {
+    return {
+      limitToFirst: () => {
+        return {
+          once: () => Promise.resolve({
+            val: () => {
+              return limitOnceHasInstance ? {} : null;
+            },
+          }),
+        };
+      },
+      set: () => Promise.resolve(),
+      child: () => {
+        return {
+          set: () => Promise.resolve(),
+          once: () => {
+            if (childOnceHasInstance) {
+              return Promise.resolve({val: () => { return {}; }});
+            }
+            return Promise.resolve({val: () => null});
+          },
+        };
+      }
+    };
+  },
+};
 
 describe('Votes Database Handler', () => {
-  // it('Sets the matches on new match db section', (done) => {
-  //   returnVal = false;
-  //   jest.spyOn(dbInstance, 'getDatabase').mockReturnValueOnce(dbMock);
-  //   const {insertMatches} = require('./matches');
+  describe('initUserVotes - ', () => {
+    it('properly resolves for all votes', (done) => {
+      childOnceHasInstance = true;
+      jest.spyOn(dbInstance, 'getDatabase').mockReturnValueOnce(dbMock);
+      const {initUserVotes} = require('./votes');
 
-  //   insertMatches([{}]).then((response) => {
-  //     expect(response).toEqual(true);
-  //     done();
-  //   });
-  // });
+      initUserVotes('foo', ['v1', 'v2']).then((response) => {
+        expect(response).toEqual(true);
+        done();
+      });
+    });
 
-  // it('sets matches on an existing match db section', (done) => {
-  //   returnVal = true;
-  //   jest.spyOn(dbInstance, 'getDatabase').mockReturnValueOnce(dbMock);
-  //   const {insertMatches} = require('./matches');
+    it('throws an error if a vote returns undefined', (done) => {
+      childOnceHasInstance = false;
+      jest.spyOn(dbInstance, 'getDatabase').mockReturnValueOnce(dbMock);
+      const {initUserVotes} = require('./votes');
 
-  //   insertMatches([{a: true}]).then((response) => {
-  //     expect(response).toEqual(true);
-  //     done();
-  //   });
-  // });
+      initUserVotes('foo', ['v1']).then((response) => {
+        expect(response).toEqual(false);
+        done();
+      });
+    });
+  });
+
+  describe('insertUserVotes', () => {
+    it('properly resolves for all votes', (done) => {
+      limitOnceHasInstance = true;
+      jest.spyOn(dbInstance, 'getDatabase').mockReturnValueOnce(dbMock);
+      const {insertUserVotes} = require('./votes');
+
+      insertUserVotes({
+        v1: { u1: true },
+        v2: { u1: true },
+      }).then((response) => {
+        expect(response).toEqual(true);
+        done();
+      });
+    });
+
+    it('updates votes if no instance found', (done) => {
+      limitOnceHasInstance = false;
+      jest.spyOn(dbInstance, 'getDatabase').mockReturnValueOnce(dbMock);
+      const {insertUserVotes} = require('./votes');
+
+      insertUserVotes({
+        v1: { u1: true },
+      }).then((response) => {
+        expect(response).toEqual(true);
+        done();
+      });
+    });
+  });
 });
